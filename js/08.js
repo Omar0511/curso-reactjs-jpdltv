1037294404e2a3dfaea6a0206688bbe554a1d7f0
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-20 00:32:05 -0600

    TEMPLATES STRING

---
+++ js/08.js
@@ -0,0 +1,6 @@
+// Templates String - Concatenación
+const producto = "Tablet";
+const precio = 499;
+const marca = "Samsung";
+
+console.log(`Producto: ${producto}, Precio: $${precio}, Marca: ${marca}`);

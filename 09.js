70bd5c5ac276c7df9ce8d32ac1b336b73ef13a71
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-21 23:17:59 -0600

    Arreglos

---
+++ 09.js
@@ -0,0 +1,7 @@
+const tecnologias = [20, 30, 40];
+console.log(tecnologias);
+console.log(tecnologias[1]);
+
+const tecnologias2 = [20, 30, 40, true, 'react.js'];
+console.log(tecnologias2);
+console.log(tecnologias2[3]);

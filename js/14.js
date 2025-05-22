ef280cd1877611262290dfc59160e88ef937d150
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-22 00:01:19 -0600

    Funciones

---
+++ js/14.js
@@ -0,0 +1,11 @@
+// Function Declaration
+function muestra() {
+    console.log(1 + 1);
+}
+muestra();
+
+// Función con parámetros
+function sumar2(n = 0, n = 0) {
+    console.log(n + n);
+}
+sumar2(4, 4);

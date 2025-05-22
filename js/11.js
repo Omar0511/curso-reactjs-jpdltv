ec88cf5e19d16c470cf01e9ded976f47ccda597a
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-21 23:40:07 -0600

    DESTRUCTURING ARRAYS

---
+++ js/11.js
@@ -0,0 +1,12 @@
+const tecnologias = ['HTML', 'CSS', 'JS'];
+
+// Destructuring Arrays
+const javascript = tecnologias[2];
+console.log(js);
+
+// Obtuene la posición del elemento
+// const [html] = tecnologias;
+console.log(html);
+
+const [ , , js] = tecnologias;
+console.log(js);

75e1b193644d0ebff4d9d981edd9eb538e97c92c
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-22 22:49:56 -0600

    Operadores OR - AND

---
+++ js/22.js
@@ -0,0 +1,12 @@
+// Operadores
+const n1 = 31;
+const n2 = 28;
+
+if (n1 > 30 || n2 < 31) {
+    console.log('Alguno de los 2 valores se cumplen');
+}
+
+
+if (n1 > 30 && n2 > 27) {
+    console.log('Ambos son mayores');
+}

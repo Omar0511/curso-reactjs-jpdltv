b5387cce95d8bcb24b6220fc492ab2f9c7bca73b
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-22 19:35:05 -0600

    Funciones que RETORNAN valores

---
+++ js/17.js
@@ -0,0 +1,12 @@
+// Funciones que retornar valores
+const sumar = (n = 0, n2 = 0) => {
+    // console.log(n + n);
+
+    return n + n2;
+};
+const resultado = sumar(31, 28);
+console.log(resultado);
+
+const sumar2 = (n1 = 0, n2 = 0) => n1 + n2;
+const resultado2 = sumar2(1, 3 );
+console.log(resultado2);

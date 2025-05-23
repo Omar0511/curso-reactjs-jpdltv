112d343144588d24bf78cb4353cb1914ade919b2
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-22 22:47:13 -0600

    Comparador estricto

---
+++ js/21.js
@@ -0,0 +1,14 @@
+const n = 1;
+const n2 = "1";
+
+if (n == n2) {
+    console.log('Iguales');
+} else {
+    console.log('No son iguales');
+}
+
+if (n === n2) {
+    console.log('No son iguales')
+} else {
+    console.log('Son iguales');
+}

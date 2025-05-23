8af8a82f828611d72cd238641493be4ad233b8bc
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-22 22:43:24 -0600

    Condicionales

---
+++ js/20.js
@@ -0,0 +1,11 @@
+// Condicionles
+const disponible = 400;
+const retirar = 50;
+
+const auth = true;
+
+if (auth) {
+    console.log('Acceso correcto');
+} else {
+    console.log('No se ha autenticado');
+}

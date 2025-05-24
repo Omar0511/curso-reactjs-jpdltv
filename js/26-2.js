51f0fe1283537fc251d451a1d24ceb7059a8f9e5
Omar Garcia <omar.garcia@morsa.com.mx>
2025-05-23 21:55:55 -0600

    MODULES

---
+++ js/26-2.js
@@ -0,0 +1,4 @@
+import { sumar, restar } from './26.js';
+
+const suma = sumar(10, 20);
+console.log(suma);
---
+++ js/26.js
@@ -0,0 +1,8 @@
+// Modules
+export function sumar(n1 = 0, n2 = 0) {
+    return n1 + n2;
+}
+
+export function restar(n1 = 0, n2 = 0) {
+    return n1 - n2;
+}
